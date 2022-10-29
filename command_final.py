bed05d3ad952bf484169198fb5e337d9712d6e55
imdevesh01 <100370515+imdevesh01@users.noreply.github.com>
2022-10-29 21:22:41 +0530

    Reminder added

--- command_final.py
+++ command_final.py
@@ -1,6 +1,7 @@
 import discord
 import random
 import wikipedia
+import asyncio
 from discord.ext import commands
 import os
 from googleapiclient.discovery import build
@@ -19,6 +20,9 @@ from datetime import datetime
 async def ping(ctx):
     await ctx.send("Pong!")
 
+@client.command()
+async def greet(ctx):
+    await ctx.send("Hey Buddy! How u doin'")
 
 #Coinflip Command
 @client.command()
@@ -188,6 +192,42 @@ async def img(ctx, *, search):
     embed1.set_image(url=url)
     await ctx.send(embed=embed1)
 
+@client.command(case_insensitive = True, aliases = ["remind", "remindme", "remind_me"])
+@commands.bot_has_permissions(attach_files = True, embed_links = True)
+async def reminder(ctx, time,*, reminder):
+    print(time)
+    print(reminder)
+    user = ctx.message.author
+    embed = discord.Embed(color=0x55a7f7)
+    seconds = 0
+    if reminder is None:
+        embed.add_field(name='Warning', value='Please specify what do you want me to remind you about.') # Error message
+    if time.lower().endswith("d"):
+        seconds += int(time[:-1]) * 60 * 60 * 24
+        counter = f"{seconds // 60 // 60 // 24} days"
+    if time.lower().endswith("h"):
+        seconds += int(time[:-1]) * 60 * 60
+        counter = f"{seconds // 60 // 60} hours"
+    elif time.lower().endswith("m"):
+        seconds += int(time[:-1]) * 60
+        counter = f"{seconds // 60} minutes"
+    elif time.lower().endswith("s"):
+        seconds += int(time[:-1])
+        counter = f"{seconds} seconds"
+    if seconds == 0:
+        embed.add_field(name='Warning',
+                        value='Please specify a proper duration, send `reminder_help` for more information.')
+    elif seconds < 300:
+        embed.add_field(name='Warning',
+                        value='You have specified a too short duration!\nMinimum duration is 5 minutes.')
+    elif seconds > 7776000:
+        embed.add_field(name='Warning', value='You have specified a too long duration!\nMaximum duration is 90 days.')
+    else:
+        await ctx.send(f"Alright, I will remind you about {reminder} in {counter}.")
+        await asyncio.sleep(seconds)
+        await ctx.send(f"Hi, you asked me to remind you about {reminder} {counter} ago.")
+        return
+    await ctx.send(embed=embed)
 
 client.run(
     "MTAxNDg3NzU1MjU1ODQ4OTczMQ.GbwkV6.ks9lmaKt7F_nDu7WI08gVODtwtpmbC66Vp-Rj0")
